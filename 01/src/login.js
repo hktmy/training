d5baa5709a41f569af42173fa045ea5075ee1513
sonedazaurus <takuya.soneda@nts.ricoh.co.jp>
2015-06-22 00:29:20 +0900

    add validation empty input

--- 01/src/login.js
+++ 01/src/login.js
@@ -1,40 +1,46 @@
 function doLogin() {
   var checkUsername = {
     minLength: 2,
-    maxLength: 8
+    maxLength: 8,
+    pattern: /[^a-z-]+/
   }
   var checkPassword = {
     minLength: 6,
-    maxLength: 24
+    maxLength: 24,
+    pattern: /[^a-zA-Z-+!@]+/
   }
   var username = document.loginForm.username.value;
   var password = document.loginForm.password.value;
-  var error_message = [];
 
-  if(username.length < checkUsername.minLength || checkUsername.maxLength < username.length) {
-    console.log('test');
-    error_message.push('usernameは２〜８文字です<br>');
+  var errorMessage = [];
+  if (username.length == 0) {
+    errorMessage.push('UserNameを入力してください<br>');
+  } else if (username.length < checkUsername.minLength || checkUsername.maxLength < username.length) {
+    errorMessage.push('UserNameは' + checkUsername.minLength + '文字以上 ' + checkUsername.maxLength + '文字以下です<br>');
   }
-  if(username.match(/[^a-z-]+/)) {
-    error_message.push('usernameに使用できる文字は 英小文字、`-` です<br>');
+  if (username.match(checkUsername.pattern)) {
+    errorMessage.push('UserNameに使用できる文字は 英小文字、`-` です<br>');
   }
-  if(password.length < checkPassword.minLength || checkPassword.maxLength < password.length) {
-    error_message.push('passwordは６〜２４文字です<br>');
+
+  if (password.length == 0) {
+    errorMessage.push('Passwordを入力してください<br>');
+  } else if (password.length < checkPassword.minLength || checkPassword.maxLength < password.length) {
+    errorMessage.push('Passwordは' + checkPassword.minLength + '文字以上 ' + checkPassword.maxLength + '文字以下です<br>');
   }
-  if(password.match(/[^a-zA-Z-+!@]+/)) {
-    error_message.push('passwordに使用できる文字は 英大小文字、`-`、`+`、`!`、`@` です<br>');
+  if (password.match(checkPassword.pattern)) {
+    errorMessage.push('Passwordに使用できる文字は 英大小文字、`-`、`+`、`!`、`@` です<br>');
   }
 
   var error = '';
-  error_message.forEach(function(msg) {
+  errorMessage.forEach(function(msg) {
     error = error + msg 
   });
 
-  if (error_message.length == 0) {
+  if (errorMessage.length == 0) {
     document.getElementById('error').innerHTML = '';
     document.loginForm.submit();
     return console.log('post success');
   } else {
-    document.getElementById('error').innerHTML = error;
+    return document.getElementById('error').innerHTML = error;
   }
 }
